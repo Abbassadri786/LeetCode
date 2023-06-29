9e86a1d0bd3802a82cf4c02550fa85060ec7f994
Abbas Sadriwala <90386455+Abbassadri786@users.noreply.github.com>
2023-06-29 14:06:20 +0530

    Update 1854. Maximum Population Year.cpp

--- 1854. Maximum Population Year.cpp
+++ 1854. Maximum Population Year.cpp
@@ -3,6 +3,7 @@ public:
     int maximumPopulation(vector<vector<int>>& logs) {
         vector<int>pop(2060);
         for(auto i: logs){
+            //increment the population count at the birth year i[0] by 1 and decrement the population count at the death year i[1] by 1.
             pop[i[0]]++;
             pop[i[1]]--;
         }
@@ -10,6 +11,7 @@ public:
         int year_index = -1;
 
         for(int i = 1950; i <= 2050; i++){
+            //update the population count by adding the population count of the previous year
             pop[i] += pop[i-1];
             if(pop[i] > maxi){
                 maxi = pop[i];
