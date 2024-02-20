568606fe624a1a7d67c4755f5a060e529a0bc956
Abbas Sadriwala <90386455+Abbassadri786@users.noreply.github.com>
2024-02-20 20:16:42 +0530

    Create 67. Add Binary.cpp

---
+++ 67. Add Binary.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        string res = "";
+        int carry = 0;
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+        
+        while (i >= 0 || j >= 0 || carry) {
+            int sum = carry;
+            if (i >= 0) sum += a[i--] - '0';
+            if (j >= 0) sum += b[j--] - '0';
+            carry = sum / 2;
+            sum %= 2;
+            res = to_string(sum) + res;
+        }
+        
+        return res;
+    }
+};
