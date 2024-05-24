f0942dd8ef910bb85b79b45f56bbb2b263a98dfb
Abbas Sadriwala <90386455+Abbassadri786@users.noreply.github.com>
2024-05-24 14:41:31 +0530

    Update 2974. Minimum Number Game.cpp

--- 2974. Minimum Number Game.cpp
+++ 2974. Minimum Number Game.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
     vector<int> numberGame(vector<int>& nums) {
-        vector<int> ans;
         sort(nums.begin(), nums.end());
-
-        for(int i=0; i<nums.size(); i+= 2){
-            ans.push_back(nums[i+1]);
-            ans.push_back(nums[i]);
+        for(int i = 0; i < nums.size(); )
+        {
+            swap(nums[i], nums[i+1]);
+            i+=2;
         }
-
-        return ans;
+        return nums;
     }
 };
