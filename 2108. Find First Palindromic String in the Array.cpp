0d699f8f8317900ca420deb083974e603d6980d6
Abbas Sadriwala <90386455+Abbassadri786@users.noreply.github.com>
2024-05-30 15:12:41 +0530

    Update 2108. Find First Palindromic String in the Array.cpp

--- 2108. Find First Palindromic String in the Array.cpp
+++ 2108. Find First Palindromic String in the Array.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
-    bool isPalindrome(string s){
+    bool isPalindrome(const string& s) {
         int start = 0;
         int end = s.length() - 1;
 
-        while(start <= end){
-            if(s[start] != s[end]){
+        while (start < end) {
+            if (s[start] != s[end]) {
                 return false;
             }
-            start++,
+            start++;
             end--;
         }
 
         return true;
     }
+
     string firstPalindrome(vector<string>& words) {
-        int n = words.size();
-        for(int i=0; i<n; i++){
-            if(isPalindrome(words[i])){
-                return words[i];
+        for (const string& word : words) {
+            if (isPalindrome(word)) {
+                return word;
             }
         }
         return "";
