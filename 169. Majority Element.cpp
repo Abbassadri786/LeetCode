6b1d54e2f0ae731691bd2c95fa8a16b2591fc641
Abbas Sadriwala <90386455+Abbassadri786@users.noreply.github.com>
2026-01-22 13:15:37 +0530

    Add Boyer-Moore algorithm for majority element
    Implemented Boyer-Moore Majority Voting Algorithm for finding the majority element.

--- 169. Majority Element.cpp
+++ 169. Majority Element.cpp
@@ -1,3 +1,29 @@
+// Aprroach 1 using Boyer-Moore Majority Voting Algorithm
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int candidate = nums[0];
+        int cnt = 1;
+
+        for(int i=1; i<nums.size(); i++){
+            if(nums[i] == candidate){
+                cnt++;
+            }
+            else{
+                cnt--;
+                if(cnt == 0){
+                    candidate = nums[i];
+                    cnt = 1;
+                }
+            }
+        }
+
+        return candidate;
+    }
+};
+
+
+//Approach 2: Using Hashmap count the freq > n/2
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
