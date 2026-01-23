026bb285b4aaa73adc41806cd2b9fcd27929890f
Abbas Sadriwala <90386455+Abbassadri786@users.noreply.github.com>
2026-01-23 19:48:22 +0530

    Refactor combination sum solution with findCombinations

--- 39. Combination Sum.cpp
+++ 39. Combination Sum.cpp
@@ -1,27 +1,27 @@
+// At each index: A) Pick the number (stay at same index → unlimited use)
+// B) Skip the number (move to next index)
 class Solution {
-public:
-    int n;
-    vector<int>nums, path;
-    vector<vector<int>> ans;
-    
-    void backtrack(int target, int start){
-        if(target == 0){
-            ans.push_back(path);
+private:
+    void findCombinations(int idx, vector<int>& arr, int target, vector<vector<int>>& ans, vector<int>& tmp){
+        if(arr.size() == idx){
+            if(target == 0){
+                ans.push_back(tmp);
+            }
             return;
         }
 
-        for(int i = start; i< n; i++){
-            if(nums[i] > target) continue;
-            path.push_back(nums[i]);
-            backtrack(target - nums[i], i);
-            path.pop_back();
+        if(arr[idx] <= target){
+            tmp.push_back(arr[idx]);
+            findCombinations(idx, arr, target - arr[idx], ans, tmp);
+            tmp.pop_back();
         }
+        findCombinations(idx+1, arr, target, ans, tmp);
     }
-
+public:
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        nums = candidates;
-        n = nums.size();
-        backtrack(target,0);
+        vector<vector<int>> ans;
+        vector<int>tmp;
+        findCombinations(0, candidates, target, ans, tmp);
         return ans;
     }
 };
