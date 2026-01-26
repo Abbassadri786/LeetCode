d169a741aff03430e387a0caa63cdd7686ba50ee
Abbas Sadriwala <90386455+Abbassadri786@users.noreply.github.com>
2026-01-26 20:16:45 +0530

    Update 46. Permutations.cpp

--- 46. Permutations.cpp
+++ 46. Permutations.cpp
@@ -1,4 +1,52 @@
 class Solution {
+private:
+    void permutations(vector<int>& tmp, vector<bool>& visited,
+                      vector<vector<int>>& ans, vector<int>& nums) {
+
+        // Base case: one full permutation formed
+        if(tmp.size() == nums.size()){
+            ans.push_back(tmp);
+            return;
+        }
+
+        // Try placing each unused number at current position
+        for(int i = 0; i < nums.size(); i++){
+
+            // Skip numbers already used in current permutation
+            if(!visited[i]){
+
+                // Choose the number
+                tmp.push_back(nums[i]);
+                visited[i] = true;
+
+                // Recurse to fill the next position
+                permutations(tmp, visited, ans, nums);
+
+                // Backtrack: undo the choice
+                tmp.pop_back();
+                visited[i] = false;
+            }
+        }
+    }
+
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>> ans;
+        vector<int> tmp;
+
+        // Keeps track of which elements are already used
+        vector<bool> visited(n, false);
+
+        permutations(tmp, visited, ans, nums);
+        return ans;
+    }
+};
+
+
+
+//Approach 2: w/o using extra space, swapping of elements
+class Solution {
 public:
     void solve(vector<int> nums, vector<vector<int>> &ans, int index){
         if(index >= nums.size()){
