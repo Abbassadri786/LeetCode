32f7bad9d1dd008d5066c9172d8f99f6e37396d4
Abbas Sadriwala <90386455+Abbassadri786@users.noreply.github.com>
2026-03-12 15:38:50 +0530

    Create 516. Longest Palindromic Subsequence.cpp

---
+++ Dynamic Programming/516. Longest Palindromic Subsequence.cpp
@@ -0,0 +1,27 @@
+class Solution {
+    // Similar to Longest Common Subsequence just initialize s2 taking reverse of s1
+public:
+    int longestPalindromeSubseq(string s1) {
+        int n = s1.length();
+        string s2 = "";
+        int end = n-1;
+        while(end >= 0){
+            s2 += s1[end--];
+        }
+
+        vector<vector<int>> dp(n+1, vector<int>(n+1, 0));
+
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n; j++){
+                if(s1[i-1] == s2[j-1]){
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+
+        return dp[n][n];
+    }
+};
