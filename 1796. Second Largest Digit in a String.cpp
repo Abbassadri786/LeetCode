541d926d5c7db11f9c215f280861969c166ee3c1
Abbas Sadriwala <90386455+Abbassadri786@users.noreply.github.com>
2026-05-09 20:19:35 +0530

    Implement function to find second largest digit

---
+++ 1796. Second Largest Digit in a String.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int secondHighest(string s) {
+        
+        // Stores largest and second largest digit
+        int first_max = -1;
+        int second_max = -1;
+
+        // Traverse entire string
+        for(int i = 0; i < s.length(); i++) {
+            
+            // Check if current character is a digit
+            if(s[i] >= '0' && s[i] <= '9') {
+                
+                int digit = s[i] - '0';
+
+                // Update largest digit
+                if(digit > first_max) {
+                    second_max = first_max;
+                    first_max = digit;
+                }
+
+                // Make sure digit is different from first_max
+                else if(digit > second_max && digit != first_max) {
+                    second_max = digit;
+                }
+            }
+        }
+
+        return second_max;
+    }
+};
