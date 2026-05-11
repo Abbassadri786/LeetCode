dd5f8a10e1ce5e4233d8fa87ce7f4f646c111fda
Abbas Sadriwala <90386455+Abbassadri786@users.noreply.github.com>
2026-05-11 23:04:13 +0530

    Implement substring count with k-frequency characters

---
+++ 3325. Count Substrings With K-Frequency Characters I.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int numberOfSubstrings(string s, int k) {
+        
+        int n = s.length();
+
+        vector<int> freq(26, 0);
+
+        int l = 0;
+        int ans = 0;
+
+        for(int r = 0; r < n; r++) {
+
+            // Expand window
+            freq[s[r] - 'a']++;
+
+            // Shrink while window is valid
+            while(freq[s[r] - 'a'] >= k) {
+
+                // All substrings from r to end are valid
+                ans += (n - r);
+
+                // Contract window
+                freq[s[l] - 'a']--;
+                l++;
+            }
+        }
+
+        return ans;
+    }
+};
