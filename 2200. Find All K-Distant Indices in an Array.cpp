65e18326f00bd4ac183063d479165cfa145fe8ee
Abbas Sadriwala <90386455+Abbassadri786@users.noreply.github.com>
2026-06-05 23:56:37 +0530

    Implement function to find k-distant indices

---
+++ 2200. Find All K-Distant Indices in an Array.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<int> findKDistantIndices(vector<int>& nums, int key, int k) {
+
+        int n = nums.size();
+        vector<int> ans;
+
+        // Check every index in the array
+        for(int i = 0; i < n; i++) {
+
+            bool valid = false;
+
+            // Search within k distance from current index
+            for(int j = max(0, i - k);
+                j <= min(n - 1, i + k);
+                j++) {
+
+                // Found a key element within range
+                if(nums[j] == key) {
+                    valid = true;
+                    break;
+                }
+            }
+
+            // Current index is k-distant from at least one key index
+            if(valid) {
+                ans.push_back(i);
+            }
+        }
+
+        return ans;
+    }
+};
