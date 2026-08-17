e871e7cea8f75c706102fb5dc7bbcd01b8d239e8
Abbas Sadriwala <90386455+Abbassadri786@users.noreply.github.com>
2026-08-17 12:27:35 +0530

    Add solution to replace '?' in string
    Implement a function to replace '?' in the string to avoid consecutive repeating characters.

---
+++ 1576. Replace All ?'s to Avoid Consecutive Repeating Characters.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string modifyString(string s) {
+        int n = s.length();
+
+        for(int i=0; i<n; i++){
+
+            if(s[i] != '?'){
+                continue;
+            }
+            // Try 'a', 'b', and 'c' and choose one
+            // different from both adjacent characters.
+            for(char ch ='a';  ch <= 'c'; ch++){
+                if((i==0 || s[i-1] != ch) && (i==n-1 || s[i+1] != ch)){
+                    s[i] = ch;
+                    break;
+                }
+            }
+        }
+        return s;
+    }
+};
