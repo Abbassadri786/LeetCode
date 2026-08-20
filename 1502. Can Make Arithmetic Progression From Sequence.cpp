984bcf55376d198b9bc2fe063777bcc37d765e82
Abbas Sadriwala <90386455+Abbassadri786@users.noreply.github.com>
2026-08-20 13:32:56 +0530

    Add function to verify arithmetic progression
    Implement a function to check if an array can form an arithmetic progression.

---
+++ 1502. Can Make Arithmetic Progression From Sequence.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool canMakeArithmeticProgression(vector<int>& arr) {
+        
+        // Sort elements to check whether they form an arithmetic progression
+        sort(arr.begin(), arr.end());
+        
+        // Common difference between the first two elements
+        int diff = arr[1] - arr[0];
+        
+        // Check if the difference remains the same
+        for(int i = 2; i < arr.size(); i++) {
+            if(arr[i] - arr[i - 1] != diff) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+};
