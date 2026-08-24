225eb4af8557e33c7c67d2170a71b4837c735018
Abbas Sadriwala <90386455+Abbassadri786@users.noreply.github.com>
2026-08-24 22:24:20 +0530

    Clean up comments in partitionString method
    Removed comments explaining the approach for clarity.

--- 2405. Optimal Partition of String.cpp
+++ 2405. Optimal Partition of String.cpp
@@ -1,10 +1,6 @@
 class Solution {
 public:
     int partitionString(string s) {
-        // so my approach
-        // inittialize map/set to keep tract of each char's ooccur
-        // iterate whole string
-        //if found any repeated char then increase count and start observing next char and repeat the same untill whole string is not traveresed
         int n = s.length();
         int cnt = 1; //Because the first partition starts as soon as the string has characters.
         unordered_map<char, int> mp;
