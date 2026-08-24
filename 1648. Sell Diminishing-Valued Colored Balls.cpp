a7ea16752fadd67a240db1725fb5883ce75275d2
Abbas Sadriwala <90386455+Abbassadri786@users.noreply.github.com>
2026-08-25 00:55:52 +0530

    Add solution for maximizing profit from colored balls
    Implement binary search to maximize profit from selling colored balls while ensuring the number of sold balls does not exceed orders.

---
+++ 1648. Sell Diminishing-Valued Colored Balls.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    bool isPossible(vector<int>&inventory, int orders, int mid){
+        // Checking if number of balls over threshold  of colours are under "orders"
+        long long count = 0;
+        for(auto a : inventory){
+            if(a > mid)
+                count += (a - mid);
+        }
+        return count <= orders;
+    }
+    int maxProfit(vector<int>& inventory, int orders) {
+        int low = 0;
+        int high = *max_element(inventory.begin(), inventory.end());
+        int k = 0;
+        int mod = (1e9) + 7;
+        while(low <= high){
+            int mid = (high - low) / 2  + low;
+            if(isPossible(inventory, orders, mid)){
+                high = mid - 1;
+                k = mid;
+            }else {
+                low = mid + 1;
+            }
+        }
+
+        // calculate the sum of all the balls of colors over threshold using Arithmetic Progression
+        int numBallsSold = 0;
+        long long totalProfit = 0;
+        for(auto ele: inventory){
+            if(ele > k){
+                int nums = ele - k;
+                // AP (first + last)*n/2 = sum of n terms
+                totalProfit = ( totalProfit + 1LL *(k+1 + ele) * nums/2 ) % mod;
+                numBallsSold += nums;
+            }
+        } 
+        if(numBallsSold < orders){
+            totalProfit  = ( totalProfit + 1LL *(orders - numBallsSold) * k ) % mod;
+        }
+        return totalProfit;
+    }
+};
