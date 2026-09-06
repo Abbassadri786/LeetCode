5be2d0cc113fc2a05c711d0479a0f7a677cbd6ed
Abbas Sadriwala <90386455+Abbassadri786@users.noreply.github.com>
2026-09-06 16:56:00 +0530

    Implement Minimum Falling Path Sum algorithm

---
+++ Dynamic Programming/931. Minimum Falling Path Sum.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int minFallingPathSum(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+
+        if (n == 1)
+            return matrix[0][0];
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+
+                if (j == 0) {
+                    matrix[i][j] += min(matrix[i - 1][j], matrix[i - 1][j + 1]);
+                }
+                else if (j == n - 1) {
+                    matrix[i][j] += min(matrix[i - 1][j], matrix[i - 1][j - 1]);
+                }
+                else {
+                    matrix[i][j] += min({matrix[i - 1][j - 1], matrix[i - 1][j], matrix[i - 1][j + 1]});
+                }
+            }
+        }
+
+        return *min_element(matrix[n - 1].begin(), matrix[n - 1].end());
+    }
+};
